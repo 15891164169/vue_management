df44091c4f10eae1ff27df2e88a707cd962ca652
milumilu <1111@333.com>
2019-11-23 17:29:01 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
-  <div id="app">
-    {{mes}}
-  </div>
+  <div id="app">app</div>
 </template>
 
 <script>
-import { login } from './network/api.js'
-export default {
-  data () {
-    return {
-      mes: '2111'
-    }
-  },
-  mounted () {
-    login({
-      username: 'admin',
-      password: '123456'
-    }).then((res) => {
-      console.log(res)
-    })
-  }
-}
+
 </script>
 
 <style scoped>
-  div {
-    color: red;
-  }
+
 </style>
